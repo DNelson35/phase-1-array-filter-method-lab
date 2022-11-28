12dabaa0b8873ec8d0551939587a5dc6742428d3
Damien Nelson <dhnisaman@gmail.com>
2022-11-28 16:32:13 -0500

    commit tree 940bf8a0586e069877c3ea27d606d9494b3b6e8b

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution here
+
+function sameUpper(item){
+    return item.charAt(0).toUpperCase() + item.slice(1)
+}
+function firstLetters(item, input){
+    return item.substring(0, input.length)
+}
+// I could have used toUpperCase on both. I thought this would be fun though
+function findMatching(arr, string){
+    const newArr = []
+    arr.filter(name => {
+        if(sameUpper(name) === sameUpper(string)){
+            newArr.push(name)
+        }
+    })
+    return newArr
+}
+
+function fuzzyMatch(arr, string){
+    const newArr = []
+    arr.filter(name => {
+        if(firstLetters(name, string) === string){
+            newArr.push(name)
+        }
+    })
+    return newArr
+}
+
+
+function matchName(arr, string){
+    const newArr = []
+    arr.filter(obj => {
+        if(obj.name === string){
+            newArr.push(obj)
+        }
+    })
+    return newArr
+}
